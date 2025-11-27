832bd33403f126d4679ac1fbec680b919c5bb676
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-11-27 23:28:02 +0100

    chore: update dependencies to use remote URLs

--- Package.swift
+++ Package.swift
@@ -216,10 +216,10 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/swift-standards/swift-cssom", from: "0.1.0"),
+        .package(url: "https://github.com/swift-standards/swift-numeric-formatting-standard", from: "0.1.0"),
         .package(url: "https://github.com/swift-standards/swift-ieee-754", from: "0.1.0"),
         .package(url: "https://github.com/swift-standards/swift-incits-4-1986", from: "0.1.0"),
         .package(url: "https://github.com/swift-standards/swift-rfc-4648", from: "0.1.0"),
-        .package(url: "https://github.com/swift-standards/swift-numeric-formatting-standard", from: "0.1.0")
     ],
     targets: [
         // MARK: - Core Modules
